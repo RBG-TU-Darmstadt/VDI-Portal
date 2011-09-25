da89c228cc8dc680c0b890b9e8bb7125a5348bec
Samuel Vogel <samuel.vogel@viison.com>
2011-09-25 15:14:37 +0200

    Document new scheduling class

--- src/management/src/main/java/vdi/management/util/Scheduling.java
+++ src/management/src/main/java/vdi/management/util/Scheduling.java
@@ -6,8 +6,16 @@ import java.util.List;
 import vdi.management.storage.DAO.NodeDAO;
 import vdi.management.storage.entities.Node;
 
+/**
+ * Bundles scheduling functionality in one place
+ */
 public class Scheduling {
 
+	/**
+	 * Chooses a NodeController.
+	 * 
+	 * @return the chosen {@link Node}
+	 */
 	public static Node selectNode() {
 		// Simply select random node
 		List<Node> nodes = NodeDAO.getNodes();
