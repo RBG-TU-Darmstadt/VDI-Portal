2e1d98edb85f3a2a9eb6122ab0116c635b9c47f4
Samuel Vogel <samuel.vogel@viison.com>
2011-09-25 21:33:49 +0200

    Add tag interface and stub resource

---
+++ src/management/src/main/java/vdi/management/rest/TagResource.java
@@ -0,0 +1,16 @@
+package vdi.management.rest;
+
+import java.util.List;
+
+import vdi.commons.web.rest.interfaces.ManagementTagService;
+import vdi.commons.web.rest.objects.ManagementTag;
+
+public class TagResource implements ManagementTagService {
+
+	@Override
+	public List<ManagementTag> getTags() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
