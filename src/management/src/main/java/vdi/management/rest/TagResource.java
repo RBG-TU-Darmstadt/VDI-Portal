e305691ea8d17e4b6249ac2ee8696af862541859
Samuel Vogel <samuel.vogel@viison.com>
2011-09-26 00:10:29 +0200

    Add comment and REST path to TagResource in management server

--- src/management/src/main/java/vdi/management/rest/TagResource.java
+++ src/management/src/main/java/vdi/management/rest/TagResource.java
@@ -3,11 +3,18 @@ package vdi.management.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Path;
+
 import vdi.commons.web.rest.interfaces.ManagementTagService;
 import vdi.commons.web.rest.objects.ManagementTag;
 import vdi.management.storage.DAO.TagsDAO;
 import vdi.management.storage.entities.Tag;
 
+/**
+ * This class exports the {@link ManagementTagService} Interface for the
+ * WebInterface, in order to receive all tags that are already available
+ */
+@Path("/tags")
 public class TagResource implements ManagementTagService {
 
 	@Override
