97ba56a97f187526abb4d375947d7623db52d01a
Lennart Diedrich <lenn@rt-diedrich.de>
2011-09-28 22:40:58 +0200

    Removed unused import and formatted annotations

--- src/management/src/main/java/vdi/management/storage/entities/User.java
+++ src/management/src/main/java/vdi/management/storage/entities/User.java
@@ -13,8 +13,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
